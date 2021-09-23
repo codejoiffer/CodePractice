c2995b89bf08fe3009c2958c59e5aa4870e9e5f0
Administrator <admin@seecoder.cn>
2021-09-23 22:53:10 +0800

    初始化

---
+++ 46-coa2021-programming02/src/main/java/util/CRC.java
@@ -0,0 +1,30 @@
+package util;
+
+public class CRC {
+
+    /**
+     * CRC计算器
+     *
+     * @param data       数据流
+     * @param polynomial 多项式
+     * @return CheckCode
+     */
+    public static char[] Calculate(char[] data, String polynomial) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * CRC校验器
+     *
+     * @param data       接收方接受的数据流
+     * @param polynomial 多项式
+     * @param CheckCode  CheckCode
+     * @return 余数
+     */
+    public static char[] Check(char[] data, String polynomial, char[] CheckCode) {
+        //TODO
+        return null;
+    }
+
+}
